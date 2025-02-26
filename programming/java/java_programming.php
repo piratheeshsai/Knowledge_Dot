d9dffea219d604743bad5aa822632c7b8a8373b0
denishdilshan <115126998+denishdilshan@users.noreply.github.com>
2025-02-26 21:22:15 +0530

    Update

--- programming/java/java_programming.php
+++ programming/java/java_programming.php
@@ -7,7 +7,7 @@
 <head>
 	<title>Unique Developer</title>
 	<!----css file link-->
-	<link rel="stylesheet" type="text/css" href="../..//css/java_programming.css">
+	<!-- <link rel="stylesheet" type="text/css" href="../..//css/java_programming.css"> -->
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -26,11 +26,13 @@
 	<!----font-awsome start-->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+	<link rel="stylesheet" type="text/css" href="css/admin.css">
+
 
 	<script src="https://apis.google.com/js/platform.js"></script>
 
 
-	<style type="text/css">
+	<!-- <style type="text/css">
 		#yt
 		{
 			margin-top: 50px;
@@ -64,14 +66,65 @@
 			color: black !important;
 		}
 
-	</style>
+	</style> -->
 </head>
 <body>
+	  <!-- Custom Navbar -->
+	  <nav class="navbar navbar-expand-lg navbar-light navbar-custom fixed-top">
+    <a class="navbar-brand" href="#">
+      <i class="fas fa-graduation-cap"></i>
+      <span>Unique Developer</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <!-- Search Form -->
+        <li class="nav-item d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100">
+          <div class="input-group position-relative">
+            <i class="fas fa-search search-icon"></i>
+            <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+          </div>
+        </li>
+        
+        <!-- Divider -->
+        <div class="topbar-divider d-none d-sm-block"></div>
+    
+       
+        
+        <!-- User Information -->
+        <li class="nav-item dropdown no-arrow">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin User</span>
+            
+          </a>
+          <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+            <a class="dropdown-item" href="#">
+              <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+              Profile
+            </a>
+            <a class="dropdown-item" href="#">
+              <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+              Settings
+            </a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="../login.php">
+              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              Logout
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
 		<!------Navigation bar ends ---->
-	<nav class="navbar navbar-inverse navbar-fixed-top" style="height: 80px;">
+<!-- <nav class="navbar navbar-inverse navbar-fixed-top" style="height: 80px;">
 		<div class="container">
 			<div class="navbar-header">
-				<!------Responsive Button---->
+				<!------Responsive Butt
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navi">
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
@@ -83,7 +136,7 @@
 				<h1 style="color: white;margin-top: 10px;" id="myhead">Unique Developer</h1>
 			</div>
 			<div class="collapse navbar-collapse" id="navi">
-                 <!------Navigation menus starts---->
+                 <!------Navigation menus starts
 				<ul class="nav navbar-nav navbar-right">
 					<li> <a href="../../index.php">Home</a></li>
 					<li> <a href="">Our Service</a></li>
@@ -93,11 +146,12 @@
 					<li> <a href="">Carrier</a></li>
 					<li> <a href="" id="our-location" class="btn-success" data-target="#mymodal" data-toggle="modal"><?php echo $_SESSION['username'];   ?></a></li>
 				</ul>
-	                 <!------Navigation menus ends---->
+	                 <!------Navigation menus end
 			</div>
 		</div>
-	</nav>
+	</nav> -->
 
+	
 		<!------Navigation bar ends ---->
 
 
