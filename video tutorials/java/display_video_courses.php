5972bd212017b70f490765cfb8a792ffbe5b59ef
Piratheesh <119719227+piratheeshsai@users.noreply.github.com>
2025-03-04 20:05:31 +0530

    last

--- video tutorials/java/display_video_courses.php
+++ video tutorials/java/display_video_courses.php
@@ -6,7 +6,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
   
   <!-- Font Awesome Icons -->
@@ -14,33 +13,6 @@
   
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
-=======
-	<title>Knowledge Dot</title>
-	<style type="text/css">
-		
-.card-body
-{
-	 border-style: solid !important;
-	  border-width:1px !important;
-	  border-color: #ccc;
-}
-
-.container
-{
-	margin-top: 1%;
-}
-
-.inner img
-{
-	transition: all 1.5s ease;  /* to rotate image smoothly*/
-	
-}
-.inner:hover img
-{
-	transform:rotate(5deg);      /* to rotate the image 5 deg*/
-	
-}
->>>>>>> 93b68f576c1d467da533e52766f607ddc9c9f3a9
 
 
   <link rel="stylesheet" type="text/css" href="../..//css/admin.css">
@@ -53,7 +25,6 @@
 <body>
 
 
-<<<<<<< HEAD
 <?php 
 
 include 'conn.inc.php';
@@ -63,12 +34,6 @@ include "../../frontend_includes/frontend_navbar.php";
 
 
 	<div class="container-fluid">
-=======
-
-	 <!-- title image start -->
-	<br> <br><br>
-	<div class="container-fluid  div1">
->>>>>>> 93b68f576c1d467da533e52766f607ddc9c9f3a9
 		
 	</div>
 	<!-- title image ends -->
@@ -82,7 +47,6 @@ include "../../frontend_includes/frontend_navbar.php";
 			
 
 <?php 
-
 
 		$sql="select * from video_info";
 		$result=mysqli_query($con,$sql);
