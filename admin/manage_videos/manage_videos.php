45e0df489d926d33416b4bbb8de4c5dfd3c8f149
SanthosN8 <santhos346@gmail.com>
2025-03-09 21:18:18 +0530

    changed quiz navigation1

--- admin/manage_videos/manage_videos.php
+++ admin/manage_videos/manage_videos.php
@@ -182,7 +182,7 @@ body {
          <ul class="list-group text-white sidebar-list">
             <li class="list-group-item  bg-dark "><a href="../admin_main.php">Welcome Admin</a></li>
             <li class="list-group-item bg-dark "><a href="manage_courses/manage_courses.php">Manage Courses</a></li>
-            <li class="list-group-item bg-dark"><a href="">Manage Quize</a></li>
+            <li class="list-group-item bg-dark"><a href="../manage_quiz/manage_quiz.php">Manage Quize</a></li>
             <li class="list-group-item bg-dark"><a href="">Manage Videos</a></li>
             <li class="list-group-item bg-dark"><a href="">Manage Comments</a></li>
             <li class="list-group-item bg-dark"><a href="">Manage Users</a></li>
