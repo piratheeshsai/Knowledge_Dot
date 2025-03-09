22f09b86d8ea6ba6a54b6c53016981375788f881
SanthosN8 <santhos346@gmail.com>
2025-03-09 20:50:18 +0530

    changed

--- admin/manage_courses/manage_courses.php
+++ admin/manage_courses/manage_courses.php
@@ -37,7 +37,8 @@ $course=$courses->display_courses();    //calling display_courses() method from 
 <nav class="navbar navbar-expand-lg navbar-light navbar-custom fixed-top">
     <a class="navbar-brand" href="#">
       <i class="fas fa-graduation-cap"></i>
-      <span>Unique Developer</span>
+      <span>Knowledge Dot
+      </span>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
