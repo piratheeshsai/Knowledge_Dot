62bbb7ffcc1fccfed4686600a6d41bd701f416a0
Ditharshan <dichuyoga@gmail.com>
2025-03-18 23:11:11 +0530

    New

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,52 +1,35 @@
-  <!-- Custom Sidebar -->
-  <div class="sidebar">
+ 
+    <!-- Custom Sidebar -->
+    <div class="sidebar">
     <ul class="sidebar-menu">
       <li class="active">
-        <a href="../admin.php">
+        <a href="#">
           <i class="fas fa-tachometer-alt"></i>
           <span>Dashboard</span>
         </a>
       </li>
       <li>
-        <a href="../manage_courses/manage_courses.php">
+        <a href="/knowledge_dot/admin/manage_courses/manage_courses.php">
           <i class="fas fa-book"></i>
           <span>Manage Courses</span>
         </a>
       </li>
       <li>
-        <a href="../manage_quiz/manage_quiz.php">
+        <a href="/knowledge_dot/admin/manage_quiz/manage_quiz.php">
           <i class="fas fa-question-circle"></i>
           <span>Manage Quiz</span>
         </a>
       </li>
       <li>
-        <a href="../manage_videos/manage_videos.php">
+        <a href="manage_videos/manage_videos.php">
           <i class="fas fa-video"></i>
           <span>Manage Videos</span>
         </a>
       </li>
       <li>
-        <a href="#">
-          <i class="fas fa-comments"></i>
-          <span>Manage Comments</span>
-        </a>
-      </li>
-      <li>
-        <a href="../admin_main.php#users">
+        <a href="#users">
           <i class="fas fa-users"></i>
-          <span>Manage Users</span>
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <i class="fas fa-chart-pie"></i>
-          <span>Reports</span>
-        </a>
-      </li>
-      <li>
-        <a href="../login.php">
-          <i class="fas fa-sign-out-alt"></i>
-          <span>Logout</span>
+          <span>View Users</span>
         </a>
       </li>
     </ul>
--- admin/manage_courses/add_new_topic.php
+++ admin/manage_courses/add_new_topic.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+  <title>Knowlade Dot</title>
 
 
   
@@ -20,12 +20,42 @@
 </head>
 
 <body>
-  
-<?php
+    <!-- Custom Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light navbar-custom fixed-top">
+    <a class="navbar-brand" href="#">
+      <i class="fas fa-graduation-cap"></i>
+      <span>Knowledge Dot</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-   require("../includes/navbar.php");    //navigation bar included
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <!-- Search Form -->
 
- ?>
+        
+        <!-- Divider -->
+        <div class="topbar-divider d-none d-sm-block"></div>
+    
+        
+        <!-- User Information -->
+        <li class="nav-item dropdown no-arrow">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin User</span>
+            
+          </a>
+          <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="../login.php">
+              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              Logout
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav
 <?php
 
 require("../includes/sidebar.php");    //navigation bar included
