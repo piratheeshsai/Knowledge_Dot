52c16b6ebbee15e194c4c4b317c9f68d3c1fcaf7
Ditharshan <dichuyoga@gmail.com>
2025-03-19 07:11:33 +0530

    nav

--- online_quize/question_show.php
+++ online_quize/question_show.php
@@ -1,38 +1,5 @@
  <!-- navigation bar starts -->
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top position-relative">  <!--  postion relative to align the tab content  -->
-  <a class="navbar-brand text-white" href="../index.php">Knowledge Dot</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto ">
-      <li class="nav-item active">
-        <a class="nav-link text-white" href="../index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <!-- <li class="nav-item">
-        <a class="nav-link text-white" href="#">Link</a>
-      </li> -->
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Our Services
-        </a>
-        <div class="dropdown-menu text-white fade" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="../programmingdemo.php">Courses</a>
-          <a class="dropdown-item" href="../video tutorials/java/display_video_courses.php">Classes</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Exams</a>
-        </div>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
         <!-- navigation bar ends -->
 <!-- ========================================================================================================================== -->
 
